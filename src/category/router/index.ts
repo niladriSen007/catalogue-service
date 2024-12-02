c619a9bd375658df44f19275804d3baa2a2e9275
Niladri <niladrisengit12@gmail.com>
2024-12-02 23:53:39 +0530

    handling error effectively

--- src/category/router/index.ts
+++ src/category/router/index.ts
@@ -9,6 +9,7 @@ import { CategoryService } from '../service';
 import CategoryValidator from '../validator';
 import { CreateCategoryRequest } from '../../common/types';
 import { logger } from '../../config/logger';
+import { requestWrapper } from '../../common/wrapper';
 const router = express.Router();
 
 const categoryService = new CategoryService();
@@ -18,13 +19,15 @@ const categoryController = new CategoryController(categoryService, logger);
 router.post(
     '/',
     CategoryValidator,
-    async (req: CreateCategoryRequest, res: Response, next: NextFunction) => {
-        (await categoryController.createCategory(
-            req,
-            res,
-            next,
-        )) as unknown as RequestHandler;
-    },
+    requestWrapper(
+        async (
+            req: CreateCategoryRequest,
+            res: Response,
+            next: NextFunction,
+        ) => {
+            await categoryController.createCategory(req, res, next);
+        },
+    ),
 );
 
 export default router;
